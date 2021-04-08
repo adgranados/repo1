ed600fa6fffc5fb805b0cbc91d45c5993407ff4e
Daniel Jimenez <djimenez@leal.co>
2021-04-08 14:50:26 -0500

    misma linea desde el master

--- saludo.py
+++ saludo.py
@@ -1,7 +1,7 @@
 print("desde la rama de cabecera")
 print("hola")
 print("adios")
-print("hola otra vez")
+print("hola otra vez, desde master.")
 print("que paso con el ultimo cambio?"
 print("esto es un cambio chevere")
 print("otra linea al final")
